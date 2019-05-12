dce1e4349cdaba124e5076aa882e1df9852ee90b
HoPGoldy <hpgless@outlook.com>
2019-05-12 21:24:31 +0800

    feat: 大厅操作

--- src/index.js
+++ src/index.js
@@ -11,16 +11,59 @@ if (!config) {
 let adb = new AdbControl()
 
 ;(async function() {
-    const result = await adb.getDevices()
-    console.log(result)
-    
-    console.log(config)
-    // await sleep(5)
+    // const result = await adb.getDevices()
+    do {
+        console.log('\n正在执行大厅操作\n')
+        await order()
+        await pickSiteMoney()
+        await pickHallMoney()
+        await clickPropaganda(20)
 
-    console.log('执行！')
-    adb.tap(390, 807)
+        console.log('\n一轮执行完成，休息 5 秒\n')
+        await sleep(5)
+    }
+    while (true)
 })()
 
 async function order() {
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 3; j++) {
+            
+            const x = config.firstSite.x + (config.siteOffset.x * j)
+            const y = config.firstSite.y + (config.siteOffset.y * i)
+            console.log(`点击第 ${(i * 3) + (j + 1)} 桌订单`)
+            adb.tap(x, y)
+            await sleep(0.5)
+        }
+    }
+}
 
+async function pickSiteMoney() {
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 3; j++) {
+            
+            const x = config.firstMoney.x + (config.siteOffset.x * j)
+            const y = config.firstMoney.y + (config.siteOffset.y * i)
+            console.log(`拾取第 ${(i * 3) + (j + 1)} 桌扔钱`)
+            adb.tap(x, y)
+            await sleep(0.5)
+        }
+    }
+}
+
+async function pickHallMoney() {
+    for (let key in config.hallMoney) {
+        const item = config.hallMoney[key]
+        console.log(`正在拾取 ${item.info} 的收入`)
+        adb.tap(item.x, item.y)
+        await sleep(0.5)
+    }
+}
+
+async function clickPropaganda(number) {
+    console.log(`\n点击 ${number} 次宣传按钮\n`)
+    for (let i = 0; i < number; i++) {
+        adb.tap(config.propaganda.x, config.propaganda.y)
+        await sleep(0.5)
+    }
 }
